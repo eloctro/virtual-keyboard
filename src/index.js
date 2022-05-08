ae8cb5cebf41b839a256acfb42899218bf17f89f
Dim <eloctro@tut.by>
2022-05-08 12:54:31 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-let i = 5;
+import { Keyboard } from './js/keybord';
+import { ButtonRows } from './js/buttonsRow';
+import { Button } from './js/button';
+
+const body = document.querySelector('body');
+// eslint-disable-next-line no-console
+new Keyboard().appendTo(body);
+
+const keyboard = document.querySelector('.keyboard');
+
+for (let rows = 0; rows < 5; rows += 1) {
+  new ButtonRows().appendTo(keyboard);
+  let buttonRows = document.querySelectorAll('.buttonRows');
+  for (let index = 0; index < 10; index += 1) {
+    new Button().appendTo(buttonRows[buttonRows.length - 1]);
+  }
+}
